61d466979ca602034ed46c9c325a9153c6bd9d45
Swati Jain <jain.swati71@gmail.com>
2024-09-02 18:54:50 +0200

    added style to input field and buttons

--- src/components/Buttons/Buttons.tsx
+++ src/components/Buttons/Buttons.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import "./style.scss";
 
 const Buttons = () => {
-  return <div>Buttons</div>;
+  return (
+    <div className="button-container">
+      <div className="button-container__row">
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="del-btn">DEL</button>
+      </div>
+      <div className="button-container__row">
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+      </div>
+      <div className="button-container__row">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>-</button>
+      </div>
+      <div className="button-container__row">
+        <button>.</button>
+        <button>0</button>
+        <button>/</button>
+        <button>*</button>
+      </div>
+      <div className="button-container__row">
+        <button className="reset-btn">RESET</button>
+        <button className="equals-btn">=</button>
+      </div>
+    </div>
+  );
 };
 
 export default Buttons;
--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Buttons from "../Buttons/Buttons";
+import "./style.scss";
 
 const Calculator = () => {
   return (
-    <div>
-      <div>input field</div>
+    <div className="calculator">
+      <header className="calculator__header">Calculator</header>
+      <input type="text" className="calculator__input" />
       <Buttons />
     </div>
   );
