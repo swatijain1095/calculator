f25d72eaf559ba6e58ae127a1454644be8fb41a4
Swati Jain <jain.swati71@gmail.com>
2024-09-02 17:12:11 +0200

    added basic components and style

---
+++ src/components/Calculator/Calculator.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Buttons from "../Buttons/Buttons";
+
+const Calculator = () => {
+  return (
+    <div>
+      <div>input field</div>
+      <Buttons />
+    </div>
+  );
+};
+
+export default Calculator;
